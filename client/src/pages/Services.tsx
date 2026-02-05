5c580e8f58559b42e21d9f13a755ca1e356cb5bc
badrabdoph-netizen <badrabdoph@gmail.com>
2026-02-05 02:15:49 +0200

    Update Services.tsx

--- client/src/pages/Services.tsx
+++ client/src/pages/Services.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Link } from "wouter";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
